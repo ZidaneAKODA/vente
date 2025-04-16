fcf044b2d4b98b5afa32a31bc99adf3433d78de0
ZidaneAKODA <mayoai556@gmail.com>
2025-04-16 08:46:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define a cache name and the files to cache
-const CACHE_NAME = 'africaphone-cache-v1';
+const CACHE_NAME = 'africaphone-cache-v2';
 const urlsToCache = [
   './index.html',
   './login.html',
