a546a6ec1de5e55bbe71eace15d7fbebc521f75a
ZidaneAKODA <mayoai556@gmail.com>
2025-06-18 21:39:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // ⚠️ Incrémentez ce nom à chaque déploiement !
-const CACHE_NAME = 'africaphone-cache-v15';
+const CACHE_NAME = 'africaphone-cache-v17';
 
 const urlsToCache = [
   '/index.html',
